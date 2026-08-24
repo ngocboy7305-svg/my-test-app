cf3d2e6a881ff215c1ba41944e275611171b8401
ngocboy7305-svg <ngocboy7305@gmail.com>
2026-08-24 14:52:59 +0700

    Create Task type

--- src/app/tabs/tasks.tsx
+++ src/app/tabs/tasks.tsx
@@ -1,12 +1,20 @@
-import Loading from "@/components/_Button/Loading";
 import { Colors, FontSize, Spacing } from "@/constants/theme";
+import { Task } from "@/type/Task";
 import { StyleSheet, Text, View } from "react-native";
-
 export default function TasksScreen() {
+    const task: Task = {
+        id: 1,
+        title: "Học React Native",
+        description: "Học React Native từ cơ bản đến nâng cao",
+        priority: "High",
+        completed: false,
+        deadline: "2022-12-31",
+        createAt: "2022-12-31",
+    }
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Công việc</Text>
-            <Loading />
+            <Text>{task.title}</Text>
+            <Text>{task.description}</Text>
         </View>
     );
 }
