704a568c67f19744f9cf38d6b76a42195c2f0a05
ngocboy7305-svg <ngocboy7305@gmail.com>
2026-08-24 16:00:46 +0700

    Create task list with FlatList

--- src/app/tabs/tasks.tsx
+++ src/app/tabs/tasks.tsx
@@ -1,20 +1,46 @@
 import TaskCard from "@/components/_Button/TaskCard";
 import { Colors, FontSize, Spacing } from "@/constants/theme";
 import { Task } from "@/type/Task";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 export default function TasksScreen() {
-    const task: Task = {
-        id: 1,
-        title: "Học React Native",
-        description: "Học React Native từ cơ bản đến nâng cao",
-        priority: "High",
-        completed: false,
-        deadline: "2022-12-31",
-        createAt: "2022-12-31",
-    }
+    const tasks: Task[] = [
+        {
+            id: 1,
+            title: "Học React Native",
+            description: "Học React Native từ cơ bản đến nâng cao",
+            priority: "High",
+            completed: false,
+            deadline: "2022-12-31",
+            createAt: "2022-12-31",
+        },
+        {
+            id: 2,
+            title: 'Học FlatList',
+            description: 'Tìm hiểu cách hiển thị danh sách',
+            priority: 'medium',
+            completed: false,
+            deadline: '26/08/2026',
+            createAt: '24/08/2026',
+        },
+
+        {
+            id: 3,
+            title: 'Làm project',
+            description: 'Tiếp tục app quản lý cá nhân',
+            priority: 'low',
+            completed: true,
+            createAt: '24/08/2026',
+        },
+    ];
     return (
         <View style={styles.container}>
-            <TaskCard task={task} />
+            <FlatList
+                data={tasks} // lấy danh sách muốn hiển thị
+                renderItem={({ item }) => <TaskCard task={item} />} // mỗi item trong danh sách sẽ được hiển thị như thế nào
+                keyExtractor={(item) => item.id.toString()} // lấy id của mỗi item để làm key
+                contentContainerStyle={styles.list} //Style cho phần nd bên trong Flatlist
+                showsVerticalScrollIndicator={false} // Ẩn thanh cuộn dọc
+            />
         </View>
     );
 }
@@ -32,5 +58,9 @@ const styles = StyleSheet.create({
         fontWeight: '700',
         color: Colors.text,
         marginBottom: Spacing.md,
+    },
+    list: {
+        padding: 20, //Khoảnh cách giữa ds và mép màn hình
+        gap: 12, //Khoảng cách giữa các card với nhau
     },
 });
